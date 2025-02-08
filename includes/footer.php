42651a400960f0bd6efe1ebb8438e35c99297b7e
Kelvin Shisanya <Kelvin.Shisanya@DDDATA.KE3>
2025-02-08 03:11:30 +0300

    art

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
       <footer class="py-lg-4 py-md-3 py-sm-3 py-3 text-center">
          <div class="copy-agile-right">
             <p> 
-              Art Gallery Management System
+            &copy; Art by Frkln
             </p>
          </div>
       </footer>
